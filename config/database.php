c97429f6962844a7d920c58abd534067dbb64c4c
mateo <letertre@et.esiea.fr>
2025-04-22 21:42:15 +0200

    change db file to deploy

--- config/database.php
+++ config/database.php
@@ -1,15 +1,21 @@
 <?php
 
-    $host = 'localhost';
-    $dbname = 'hairlab_db';
-    $username = 'root';
-    $password = '';
+    $host = getenv('MYSQLHOST');
+    $port = getenv('MYSQLPORT');
+    $dbname = getenv('MYSQLDATABASE');
+    $username = getenv('MYSQLUSER');
+    $password = getenv('MYSQLPASSWORD');
 
     try {
-        $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]);
+        $pdo = new PDO(
+            "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4",
+            $username,
+            $password,
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]
+        );
     } catch (PDOException $e) {
         die("Erreur de connexion : " . $e->getMessage());
     }
